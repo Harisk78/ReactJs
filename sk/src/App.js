cc88a96548f7905519c07b4770d62f75695d6c17
KPT <placement@lab.com>
2024-02-08 17:40:02 +0530

    Import modified

--- sk/src/App.js
+++ sk/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import React from "react";
-import Header from "./components/Header.js";
-import Content from "./components/Content.js";
-import Footer from "./components/Footer.js";
+import Header from "./components/Header/Header.js";
+import Content from "./components/Content/Content.js";
+import Footer from "./components/Footer/Footer.js";
 function App() {
   return (
     <div className="App">
