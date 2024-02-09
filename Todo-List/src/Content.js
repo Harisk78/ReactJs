dd00c3be8919a7ebec376a45d11a6f0cca240b23
KPT <placement@lab.com>
2024-02-09 18:26:26 +0530

    There is a problem in ToDoList

--- Todo-List/src/Content.js
+++ Todo-List/src/Content.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import './App.css'
 const Content = () => {
+    let input = document.getElementById('input');
+    let con = document.getElementById('container');
     function add(){
-        let li = document.createElement()
+        // let list = document.createElement("li");
+        // list.innerHTML = inp.value;
+        // console.log(list);    
+        // inp.value="";
+        console.log(input)
     }
   return (
     <div className='con'>
         <fieldset>
             <div className='inp'>
-                <input type="text" placeholder='Enter Task'/>
+                <input type="text" placeholder='Enter Task' id='input' />
                 <button onClick={add}>Add</button>
             </div>
-            <div id='container'>
-                <ul>
-                    <li>Eat</li>
-                    <li>Code</li>
-                    <li>Repeat</li>
-                </ul>
-            </div>
+            <ul id="container">
+                <li></li>
+            </ul>
         </fieldset>
     </div>
   )
